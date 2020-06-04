48e3ebbe0ddfe680a443443e016f6c1a41d074b7
Fabiano Dias <fabiano.dias@trsolucoes.com>
2020-06-04 18:15:41 -0300

    api change

--- src/config.js
+++ src/config.js
@@ -3,12 +3,12 @@ import _ from 'lodash';
 
 
 const config = {
-  apiKey: null,
-  apiSecret: null,
-  amount: 100,
+  apiKey: 'fc4ba69e8259482782a8263283287e9a41fce0abdf6d81c64caf6845163cba05',
+  apiSecret: '527658d20de29cb4d388a9816c4622f92bbed87736d6a0ec40d9a962a12aceb9',
+  amount: 1000,
   amountCurrency: 'BRL',
   initialBuy: true,
-  minProfitPercent: 0.02,
+  minProfitPercent: 0.01,
   // specify null to let the bot calculate the minimum allowed interval
   intervalSeconds: null,
   playSound: false,
