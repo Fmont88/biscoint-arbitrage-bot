8f195b5eda387fbc0aba5167df5531a9d6dc44fb
Fmont88 <64174359+Fmont88@users.noreply.github.com>
2020-08-03 13:22:32 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ import _ from 'lodash';
 const config = {
   apiKey: 'fc4ba69e8259482782a8263283287e9a41fce0abdf6d81c64caf6845163cba05',
   apiSecret: '527658d20de29cb4d388a9816c4622f92bbed87736d6a0ec40d9a962a12aceb9',
-  amount: 2500,
+  amount: 5000,
   amountCurrency: 'BRL',
   initialBuy: true,
   minProfitPercent: 0.01,
